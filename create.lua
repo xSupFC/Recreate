657382651a5e1b7db64d5626b0597e27aecc52e2
xSupFC <109413786+xSupFC@users.noreply.github.com>
2022-11-21 21:48:41 +0800

    Add files via upload

---
+++ create.lua
@@ -0,0 +1,9 @@
+--[[
+  Got You Skid. You thought it was the script lmao. hard?
+]]
+
+game.StarterGui:SetCore("SendNotification", {
+    Title = "Andesite Obfuscator";
+    Text = "Script Protected 🔒";
+    Duration = 10;
+})
